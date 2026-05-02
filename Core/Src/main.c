07b450545919f20041b30ad908c78f2ecd3a2e5c
Yousef Samet <yoosefsamet@gmail.com>
2026-05-02 22:10:37 +0330

    [test] add simple task

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,8 +1,12 @@
+#include "FreeRTOSTasks.h"
+
 #include "flash.h"
 #include "rcc.h"
 #include "gpio.h"
 
 #define WAITE_STATE (0x00U) /* True latency for HSI (8MHz) SYSCLK clock */
+
+void StartTask( void *param );
 
 int main( void )
 {
@@ -14,9 +18,21 @@ int main( void )
 
 	GPIO_Init();
 
+	( void )xTaskCreate(StartTask, "Task1", STARTUP_STACK_SIZE, NULL, STARTUP_STACK_PRIORITY, NULL);
+
+	return 0;
+}
+
+void StartTask( void *param )
+{
+	( void )param;
+
 	while (1)
 	{
-		
+		GPIOB->BSRR |= GPIO_BSRR_BS15;
+		vTaskDelay(pdMS_TO_TICKS(500));
+
+		GPIOB->BRR |= GPIO_BRR_BR15;
+		vTaskDelay(pdMS_TO_TICKS(500));
 	}
-	
 }
